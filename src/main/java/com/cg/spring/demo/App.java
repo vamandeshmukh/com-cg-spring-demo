110f2cf6c6a58151a823ee9a568a3af596524b8d
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-30 15:59:25 +0530

    Commti
    Commit

--- src/main/java/com/cg/spring/demo/App.java
+++ src/main/java/com/cg/spring/demo/App.java
@@ -1,6 +1,7 @@
 package com.cg.spring.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -14,10 +15,15 @@ public class App {
 		System.out.println("Start");
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
-		Employee emp = context.getBean(Employee.class);
+		Employee emp = context.getBean("employee", Employee.class);
 		emp.work();
 		System.out.println(emp.ename);
 
+		Employee emp2 = context.getBean("employee2", Employee.class);
+		emp2.work();
+		System.out.println(emp2.ename);
+
 		System.out.println("End");
+		((AbstractApplicationContext) context).close();
 	}
 }
--- src/main/java/com/cg/spring/demo/Employee.java
+++ src/main/java/com/cg/spring/demo/Employee.java
@@ -2,7 +2,33 @@ package com.cg.spring.demo;
 
 public class Employee {
 
-	public String ename;
+	private int eid;
+	private String ename;
+	private double salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String ename) {
+		super();
+		this.ename = ename;
+	}
+
+	public Employee(int eid, String ename, double salary) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.salary = salary;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
 
 	public String getEname() {
 		return ename;
@@ -12,8 +38,21 @@ public class Employee {
 		this.ename = ename;
 	}
 
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
 	public void work() {
 		System.out.println("Employee works...");
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + "]";
 	}
 
 }
