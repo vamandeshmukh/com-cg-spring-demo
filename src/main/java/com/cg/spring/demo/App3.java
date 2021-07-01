c723e36e37234f89eb51dff40d97847234edac6d
vamandeshmukh <vamandeshmukh@gmail.com>
2021-07-01 12:07:48 +0530

    C
    C

--- src/main/java/com/cg/spring/demo/App3.java
+++ src/main/java/com/cg/spring/demo/App3.java
@@ -2,17 +2,20 @@ package com.cg.spring.demo;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.AbstractApplicationContext;
 
 // Annotation based Spring configuration 
 
+@ComponentScan
 public class App3 {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-		Employee emp = context.getBean(Employee.class);
-		System.out.println(emp.toString());
+		ApplicationContext context = new AnnotationConfigApplicationContext(App3.class);
+
+		Customer cust1 = context.getBean(Customer.class);
+		cust1.buy();
 
 		((AbstractApplicationContext) context).close();
 
