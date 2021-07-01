808accc9184267160d6a9082dbd2c3957815d42e
vamandeshmukh <vamandeshmukh@gmail.com>
2021-07-01 10:43:30 +0530

    Commit
    Commit

--- src/main/java/com/cg/spring/demo/App.java
+++ src/main/java/com/cg/spring/demo/App.java
@@ -14,7 +14,10 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
+		System.out.println("Create spring container object");
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+
+		System.out.println("Property injection / Setter injection");
 		Employee emp = context.getBean("employee", Employee.class);
 		emp.work();
 		System.out.println(emp.toString());
@@ -22,22 +25,31 @@ public class App {
 		Employee emp2 = context.getBean("employee2", Employee.class);
 		System.out.println(emp2.toString());
 
+		System.out.println("Constructor injection");
 		Employee emp3 = context.getBean("employee3", Employee.class);
 		System.out.println(emp3.toString());
 
 		Employee emp4 = context.getBean("employee4", Employee.class);
 		System.out.println(emp4.toString());
 
+		System.out.println("Inner bean - using property injection ");
 		Employee emp5 = context.getBean("employee5", Employee.class);
 		System.out.println(emp5.toString());
 
+		System.out.println("Inner bean - using constructor injection");
 		Employee emp6 = context.getBean("employee6", Employee.class);
 		System.out.println(emp6.toString());
 
+		System.out.println("Collection injection - using property injection ");
 		Department dept3 = context.getBean("department3", Department.class);
 		System.out.println(dept3.toString());
 
-		System.out.println("End");
+		System.out.println("Collection injection in inner bean - using constructor injection ");
+		Employee emp7 = context.getBean("employee7", Employee.class);
+		System.out.println(emp7.toString());
+
 		((AbstractApplicationContext) context).close();
+
+		System.out.println("End");
 	}
 }
