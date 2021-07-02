6b13aad01c22a0082311ec0de220dc321d08570f
vamandeshmukh <vamandeshmukh@gmail.com>
2021-07-02 10:28:40 +0530

    Com
    Com

--- src/main/java/com/cg/spring/demo/db/App.java
+++ src/main/java/com/cg/spring/demo/db/App.java
@@ -1,7 +1,12 @@
 package com.cg.spring.demo.db;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.cg.spring.demo.db.model.Employee;
 import com.cg.spring.demo.db.service.EmployeeService;
 
 /**
@@ -25,6 +30,22 @@ public class App {
 
 		EmployeeService service = context.getBean(EmployeeService.class);
 		System.out.println(service.getEmployeebyId(101).toString());
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please enter eid");
+		int eid = sc.nextInt();
+		System.out.println("Please enter ename");
+		String ename = sc.next();
+		System.out.println("Please enter salary");
+		double salary = sc.nextDouble();
+		Employee emp = new Employee(eid, ename, salary);
+		service.insertEmployee(emp);
+		System.out.println("All employees:");
+		List<Employee> empList = service.getAllEmployees();
+
+		for (Employee e : empList)
+			System.out.println(e.toString());
 
 		context.close();
 
